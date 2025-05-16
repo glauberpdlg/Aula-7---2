e3bfaf45a282a30229b8d56384f5b4c07fe9addd
Glauber Mathias <glauber.pdlg@gmail.com>
2025-05-16 20:58:58 -0300

    Add files via upload

---
+++ desafio3.py
@@ -0,0 +1,36 @@
+def soma(a,b):
+    s = a + b
+    print("{} + {} = {}".format(a,b,s))
+
+def subtrai(a,b):
+    st = a - b
+    print("{} - {} = {}".format(a,b,st))
+
+def mult(a,b):
+    m = a * b
+    print("{} * {} = {}".format(a,b,m))
+
+def div(a,b):
+    d = a / b
+    print("{} / {} = {}".format(a,b,d))
+
+while True:
+    def calculadora(a,b):
+        operacoes = int(input("\n1- soma \n2- subtração \n3- multiplicação \n4- divisão \nEscolha sua operação desejada: "))
+        if operacoes == 1:
+            soma(a,b)
+        elif operacoes == 2:
+            subtrai(a,b)
+        elif operacoes == 3:
+            mult (a,b)
+        elif operacoes == 4:
+            div (a,b)
+    
+    f = int(input("Digite o primeiro número: "))
+    
+    g = int(input("Digite o segundo número: "))
+
+    print(100*"-")
+
+    calculadora(f,g)
+ 
